b9402a16032bdc7f91c2d319c896aa7aadc782bd
nichro02 <nichro02@gmail.com>
2021-02-09 17:59:14 -0700

    Added routes to retrieve all comments, retrieve comments for one player, and delete one comment

--- resources/comments.py
+++ resources/comments.py
@@ -22,8 +22,39 @@ def new_comment():
         return jsonify(data={}, status={'code': 404, 'message': 'Error getting resources'})
 
 
-#GET ROUTE TO DISPLAY COMMENTS
+#GET ROUTE TO DISPLAY COMMENTS ON LANDING PAGE
+@comments.route('/get_all', methods=['GET'])
+def get_all():
+    try:
+        get_query = models.Comment.select().order_by(models.Comment.id.desc())
+        comments_to_dict = [model_to_dict(comments) for comments in get_query]
+        for item in comments_to_dict:
+            del item['user_id']['password']
+        return jsonify(data=comments_to_dict, status={'code': 201, 'message': 'scores returned'})
+    except models.DoesNotExist:
+        return jsonify(data={}, status={'code': 404, 'message': 'Error getting resources'})
+
+#GET ROUTE TO DISPLAY COMMENTS ON PROFILE PAGE
+@comments.route('/get_comments/<player_id>', methods=['GET'])
+def get_user_comments(player_id):
+    try:
+        user_comments_query = models.Comment.select().where(models.Comment.user_id == player_id)
+        comments_to_dict = [model_to_dict(comments) for comments in user_comments_query]
+        for item in comments_to_dict:
+            del item['user_id']['password']
+        return jsonify(data=comments_to_dict, status={'code': 201, 'message': 'scores returned'})
+    except models.DoesNotExist:
+        return jsonify(data={}, status={'code': 404, 'message': 'Error getting resources'})
+
 
 #PUT ROUTE TO UPDATE COMMENTS
 
 #DELETE ROUTE
+@comments.route('/<comment_id>', methods=['Delete'])
+def delete_one(comment_id):
+    try:
+        comment_to_delete = models.Comment.get_by_id(comment_id)
+        comment_to_delete.delete_instance()
+        return jsonify(data={}, status={'code': 200, 'message': 'Record successfully deleted'})
+    except models.DoesNotExist:
+        return jsonify(data={}, status={'code': 404,'message': 'Error getting resources'})
